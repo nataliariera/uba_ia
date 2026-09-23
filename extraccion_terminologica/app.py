8f41d0ce8b62e75cdf63fb70901fa0500936bd08
Natalia Riera <nsriera@gmail.com>
2026-09-23 10:13:48 -0300

    Update app.py

--- extraccion_terminologica/app.py
+++ extraccion_terminologica/app.py
@@ -106,7 +106,7 @@ def extraer_candidatos(
 
 
 def df_a_csv_bytes(df: pd.DataFrame) -> bytes:
-    return df.to_csv(index=False, encoding="utf-8-sig").encode("utf-8-sig")
+    return df.to_csv(index=False, sep=";").encode("utf-8-sig")
 
 
 # ---------------------------------------------------------------------------
